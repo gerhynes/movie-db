8d937cce7f8e76839a13d3ceebc5eeffd3fd4870
Gerard Hynes <gkhynes@gmail.com>
2020-01-26 12:23:21 +0000

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
-import React, { Component } from "react";
-import Movie from "./Movie";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import MoviesList from "./MoviesList";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    movies: []
-  };
-  async componentDidMount() {
-    try {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=b698d228ba0c27131c9c6ff708907bf9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
-      );
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <h1 className="App-title">{"< MOVIE DB >"}</h1>
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={Test} />
+      </Switch>
+    </div>
+  </Router>
+);
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>{"< MOVIE DB >"}</h1>
-        </header>
-        {this.state.movies.map(movie => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
-      </div>
-    );
-  }
-}
+const Test = ({ match }) => <h1>{match.params.id}</h1>;
 
 export default App;
